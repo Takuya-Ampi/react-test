83892b96c08f561730a0db63112fd1d77eb308dc
Takuya-Ampi <53106671+Takuya-Ampi@users.noreply.github.com>
2021-05-13 09:55:47 +0900

    メソッド追加およびstyle追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert("aaa");
+  const contentStyle = { color: "blue", fontSize: "18px" };
   return (
     <>
-      <h1>Hello</h1>
-      <p>拡張子変更</p>
+      <h1 style={{ color: "red", fontSize: "24px" }}>Hello</h1>
+      <p style={contentStyle}>拡張子変更</p>
+      <button onClick={onClickButton}>クリック</button>
     </>
   );
 };
