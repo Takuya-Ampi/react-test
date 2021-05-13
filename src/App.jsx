b2279f77b954bcb2cc152319847fdd2f1929e473
Takuya-Ampi <53106671+Takuya-Ampi@users.noreply.github.com>
2021-05-13 22:48:28 +0900

    props.childrenの書き方

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
       <h1 style={{ color: "red", fontSize: "24px" }}>Hello</h1>
-      <ColorfulMessage color="blue" message="お元気ですか？" />
+      <ColorfulMessage color="blue">新しいメッセージ</ColorfulMessage>
       <ColorfulMessage color="pink" message="元気です" />
       <button onClick={onClickButton}>クリック</button>
     </>
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ColorfulMessage = (props) => {
   console.log(props);
   const contentStyle = { color: props.color, fontSize: "18px" };
-  return <p style={contentStyle}>{props.message}</p>;
+  return <p style={contentStyle}>{props.children}</p>;
 };
 
 export default ColorfulMessage;
