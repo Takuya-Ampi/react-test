a0642a746c16e21bc118d6d4985390be93466da0
Takuya-Ampi <53106671+Takuya-Ampi@users.noreply.github.com>
2021-05-13 23:04:48 +0900

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert("aaa");
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
 
   return (
     <>
       <h1 style={{ color: "red", fontSize: "24px" }}>Hello</h1>
       <ColorfulMessage color="blue">新しいメッセージ</ColorfulMessage>
       <ColorfulMessage color="pink">Yeah</ColorfulMessage>
-
-      <button onClick={onClickButton}>クリック</button>
+      <button onClick={onClickCountUp}>クリック</button>
+      <p>{num}</p>
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
-  console.log(props);
   const { color, children } = props;
   const contentStyle = { color, fontSize: "18px" };
   return <p style={contentStyle}>{children}</p>;
