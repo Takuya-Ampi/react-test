f43d2a4bd48dd9296680a48325708dacbf02b940
Takuya-Ampi <53106671+Takuya-Ampi@users.noreply.github.com>
2021-05-13 22:46:45 +0900

    props 使ってstyleとテキスト動的に表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   const onClickButton = () => alert("aaa");
-  const contentStyle = { color: "blue", fontSize: "18px" };
+
   return (
     <>
       <h1 style={{ color: "red", fontSize: "24px" }}>Hello</h1>
-      <p style={contentStyle}>拡張子変更</p>
+      <ColorfulMessage color="blue" message="お元気ですか？" />
+      <ColorfulMessage color="pink" message="元気です" />
       <button onClick={onClickButton}>クリック</button>
     </>
   );
